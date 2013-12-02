2f64b7e1032901d522dbbeefd36950cb4f459512
Thomas Levine <occurrence@thomaslevine.com>
2013-12-02 02:34:06 +0000

    moar

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 var request = require('request')
+  , url     = require('url')
 
 function main() {
   var APIKEY
   if (! (APIKEY = process.env.APIKEY)) {
     console.log('You need to set the APIKEY environment variable to your 3Taps API key.')
     process.exit(1)
+  } else {
+    search3Taps(2, )
   }
+
 }
 
-"http://search.3taps.com?auth_token=$APIKEY&location.state=USA-NY&category=RSUB"
+function search3Taps(rpp, callback){
+  var apiUrl = "http://search.3taps.com?auth_token=" + APIKEY + \
+    "&SOURCE=CRAIG&location.state=USA-NY&category=RSUB&retvals=id&rpp=" + rpp
+
+  requests.get({"url":apiUrl, json:true}, function(err, res, body) {
+    if (err) {
+      console.log('Error on ' + url)
+      console.log(err)
+      console.log(res)
+      console.log(body)
+      console.log('')
+    } else {
+      return callback(err, res, body)
+  })
+}
+
+function downloadCraigslist(craigslistUrl) {
+  url.parse(craigslistUrl)
+}
 
 main()
