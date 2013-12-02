af666c3ef3803c63998f781d076e33e95ba47af8
Thomas Levine <occurrence@thomaslevine.com>
2013-12-02 16:32:29 +0000

    stub

--- is_date_range.py
+++ is_date_range.py
@@ -4,7 +4,10 @@ import os
 import lxml.html
 import pandas
 
-df = pandas.read_csv(os.path.join('fixtures','fixtures.csv')).head()
-df.index = df['url'].map(lambda url: int(url.split('/')[-1].split('.')[0]))
-df['filename'] = df['url'].map(lambda url: os.path.join('fixtures',url.split('/')[-1]))
-df['html'] = df['filename'].map(lambda x: lxml.html.parse(x).getroot())
+def main():
+    df = pandas.read_csv(os.path.join('fixtures','fixtures.csv')).head()
+    df.index = df['url'].map(lambda url: int(url.split('/')[-1].split('.')[0]))
+    df['filename'] = df['url'].map(lambda url: os.path.join('fixtures',url.split('/')[-1]))
+    df['html'] = df['filename'].map(lambda x: lxml.html.parse(x).getroot())
+
+def is_date(string):
