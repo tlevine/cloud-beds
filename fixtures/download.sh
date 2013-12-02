4020f21a482fcebeabc6c2a47285f9d979cd7eec
Thomas Levine <occurrence@thomaslevine.com>
2013-12-02 01:15:51 +0000

    download

--- fixtures/download.sh
+++ fixtures/download.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 for fixture in $(cat fixtures.csv|sed 1d|cut -d, -f1); do
-  echo $fixture
+  filename=$(echo $fixture | sed 's/^.*\///')
+  wget -O $filename $fixture
+  sleep $(( $RANDOM / 1000 ))
 done
