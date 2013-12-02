8332bbea069af5127497071430800fcd02979928
Thomas Levine <occurrence@thomaslevine.com>
2013-12-02 16:39:08 +0000

    docs

--- is_date_range.py
+++ is_date_range.py
@@ -10,4 +10,21 @@ def main():
     df['filename'] = df['url'].map(lambda url: os.path.join('fixtures',url.split('/')[-1]))
     df['html'] = df['filename'].map(lambda x: lxml.html.parse(x).getroot())
 
-def is_date(string):
+def is_date(tokens: list): -> bool
+    '''
+    Given a list of words, determine whether the list
+    collectively forms a single date.
+
+    >>> is_date('From January')
+    False
+
+    >>> is_date('January')
+    True
+
+    >>> is_date('January 3')
+    True
+
+    >>> is_date('January 3 to')
+    False
+
+    '''
--- test/test_is_date.py
+++ test/test_is_date.py
@@ -2,12 +2,23 @@ from is_date_range import is_date
 import nose.tools as n
 
 def check_is_date(string, expectation):
-    n.assert_equal(is_date(string), expectation)
+    n.assert_equal(is_date(string.split(' ')), expectation)
 
 testcases = [
-    (,)
-    (,)
-    (,)
+    ('January 6 - January 31',False)
+    ('January 6 - January',False)
+    ('January 6',True)
+    ('January',True)
+    ('January 31 (Rego',False)
+    ('1br - December to January Sublet in bedstuy',False)
+    ('1br',False)
+    ('December to January',False)
+    ('January Sublet',False)
+    ('December to',False)
+    ('December',True)
+    ('December 19th to January',False)
+    ('19th to',False)
+    ('December 19th to January 18th',False)
     (,)
     (,)
     (,)
