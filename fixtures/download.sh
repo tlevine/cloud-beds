dbd298dd925f3059ff6a3d1ece85f0a9531987e0
Thomas Levine <occurrence@thomaslevine.com>
2014-01-21 16:05:27 +0000

    copy fixtures rather than downloading anew

--- fixtures/download.sh
+++ fixtures/download.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
+set -e
 for fixture in $(cat fixtures.csv|sed 1d|cut -d, -f1); do
   filename=$(echo $fixture | sed 's/^.*\///')
   if ! test -e $filename; then
-    echo Beginning download of $fixture
-    wget --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0' -O $filename $fixture
-    echo Finished downloading $fixture
-    sleep $(( $RANDOM / 1000 ))
+    echo Copying $fixture
+    cp ../craigslist/$(echo "$fixture"|sed 's+https\?://++') .
   fi
-    echo Already downloaded $fixture
 done
