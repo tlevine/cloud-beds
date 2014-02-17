97e48e53ec13c43d47d543700e1e8dccf82a3d27
Thomas Levine <_@thomaslevine.com>
2014-02-17 01:39:53 +0000

    delete

--- index.py
+++ index.py
@@ -37,70 +37,15 @@ def main():
         t.start()
 
 def search_subdomain(subdomain):
-    for url in Search(subdomain):
-        get(url)
+    for listing in Section(subdomain, 'sub'):
+        print(listing)
         break
-
-class Search:
-    def __init__(self, subdomain):
-        self.subdomain = subdomain
-
-    def __iter__(self):
-        self.buffer = []
-        self.html = None
-        self.present_search_url = None
-        return self
-
-    def __next__(self):
-        if self.buffer == []:
-            self.download()
-            self.buffer.extend(map(str,self.html.xpath('//p[@class="row"]/a/@href')))
-
-        if self.html.xpath('count(//p[@class="row"])') == 0:
-            logger.debug('Stopped at %s' % self.present_search_url)
-            raise StopIteration
-        else:
-            return self.buffer.pop(0)
-
-    def download(self):
-        self.present_search_url = self.next_search_url()
-        fp = get(self.present_search_url)
-
-        html = lxml.html.fromstring(fp.read())
-        html.make_links_absolute(self.present_search_url)
-        self.html = html
-
-    def next_search_url(self):
-        'Determine the url of the next search page.'
-        if not self.html:
-            return 'https://%s.craigslist.org/sub/index000.html' % self.subdomain
-
-        nexts = set(self.html.xpath('//a[contains(text(),"next >")]/@href'))
-        if len(nexts) != 1:
-            raise ValueError('No next page for %s' % self.search_url)
-        return str(list(nexts)[0])
-
-
-def price(text):
-    'Find the price of a listing. Use the highest dollar value in the listing.'
-    numbers = re.findall(r'[$0-9]+', re.sub(r'[, ]', '', text))
-    monies = filter(lambda x: '$' in x, numbers)
-    integers = list(_ints(money.replace('$','') for money in monies))
-    if len(integers) > 0:
-        return max(integers)
 
 def furnished(text):
     return 'furnished' in text and not 'unfurnished' in text
 
-def _ints(monies):
-    for money in monies:
-        try:
-            yield int(money)
-        except ValueError:
-            pass
-
 def weekly(html):
     return 'week' in html.text_content()
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
