6ef2a1838d0bd822cf6be420815d72abdcd7b768
Thomas Levine <_@thomaslevine.com>
2014-02-22 01:23:37 +0000

    graph.r

--- graph.r
+++ graph.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 
 if (!('s' %in% ls())) {
-  s <- read.csv('/tmp/sublets.csv')
 }
+s <- read.csv('/tmp/sublets.csv', stringsAsFactors = FALSE, na.strings = '')
+subset(s, !is.na(s$start))
