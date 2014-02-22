2494d559b53e95d898bdec92cf477f2b84f7ab23
Thomas Levine <_@thomaslevine.com>
2014-02-22 01:48:46 +0000

    graph

--- graph.r
+++ graph.r
@@ -1,6 +1,10 @@
 library(ggplot2)
 
 s <- read.csv('/tmp/sublets.csv', stringsAsFactors = FALSE, na.strings = '')
-candidates <- subset(s, !is.na(s$start) & subdomain == 'austin' & section == 'sub' & end <= 4)
-candidates <- candidates[order(candidates$posted),]
-print(candidates[c('price','start','end','url')])
+
+candidates <- subset(s, !is.na(s$start) & subdomain == 'austin' & section == 'sub' & start >= 2 & end <= 4 & !is.na(updated))
+for (col in c('posted','updated')) {
+  candidates[,col] <- as.POSIXct(candidates[,col])
+}
+candidates <- candidates[order(candidates$updated - candidates$posted),]
+print(candidates[c('posted', 'price','start','end','url')])
