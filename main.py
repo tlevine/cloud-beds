63d61ed945f82c1cfbca6df56ce188c15cf21ffe
Thomas Levine <_@thomaslevine.com>
2014-03-03 21:00:05 +0000

    aoeu

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from craigsgenerator.generators import sites, listings
 from craigsgenerator import craigsgenerator
+import sqlalchemy
 
 from config import proxies
+
+DATABASES = [
+    'sqlite://///cloud-sleeping.db',
+]
+
+def main():
+    cg = craigsgenerator(get = get, threads_per_section = 10, superthreaded = False,
+        cachedir = os.path.join(os.environ['HOME'], 'dadawarehouse', 'craigslist'))
+
+    for listing in cg:
+        save(listing)
 
 def get(url):
     return requests.get(url, proxies = proxies)
 
-def craigsgenerator(sites = lambda: sites(get = get), listings = lambda: listings(get = get),
-                    sections = ['sub'], threaded = True):
+def save(listing, targets = DATABASES):
+    databases = {target:sqlalchemy.create_engine(target) for target in targets}
