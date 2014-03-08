67ca67af0f2ab670ee85afbb77aa361a15e77c52
Thomas Levine <_@thomaslevine.com>
2014-03-08 22:16:04 +0000

    test

--- cloud_beds/cli.py
+++ cloud_beds/cli.py
@@ -6,7 +6,7 @@ import requests
 
 from cloud_beds.db import db, get_session
 
-def main():
+def get_generator():
     if 'https_proxy' in os.environ:
         proxies = {'https': os.environ['https_proxy']}
     else:
@@ -17,10 +17,12 @@ def main():
     sections = ['sub']
     sites = ['philadelphia.craigslist.org']
 
-    cg = craigsgenerator(get = get, threads_per_section = 10, superthreaded = False,
-                         sites = sites, sections = sections,
-                         cachedir = cachedir)
+    return craigsgenerator(get = get, threads_per_section = 10, superthreaded = False,
+                           sites = sites, sections = sections,
+                           cachedir = cachedir)
 
+def main():
+    cg = get_generator()
     sink = db(get_session(database))
     next(sink)
 
--- cloud_beds/test/test_cli.py
+++ cloud_beds/test/test_cli.py
@@ -1,2 +1,6 @@
-def test_import():
-    import cloud_beds.cli
+import cloud_beds.cli as cli
+
+import nose.tools as n
+
+def test_get_generator():
+    n.assert_true(hasattr(cli.get_generator(), '__next__'))
