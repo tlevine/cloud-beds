aca560d736e0faa56623476f36671011e510f918
Thomas Levine <_@thomaslevine.com>
2014-03-08 19:01:22 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='cloud-beds',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Find beds in the cloud.',
+    url='https://github.com/tlevine/cloud-beds.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['cloud_beds'],
+    install_requires = ['requests','craigsgenerator'],
+    tests_require = ['nose'],
+    version='0.0.1',
+    license='AGPL'
+)
